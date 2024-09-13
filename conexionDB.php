53c917b5cbc2ba101593d92698633dd8f97ec793
hector-14 <63135688+hector-14@users.noreply.github.com>
2024-09-13 16:33:51 -0600

    Update conexionDB.php

--- conexionDB.php
+++ conexionDB.php
@@ -1,3 +1,9 @@
 <?php
-    $conexion = mysqli_connect("localhost","root","","datos"); 
+    session_start();
+    $db_host = $_ENV["db_host"];
+    $db_user = $_ENV["db_user"];
+    $db_password = $_ENV["db_password"];
+    $db_name = $_ENV["db_name"];
+    $db_port = $_ENV["db_port"];
+    $conexion = mysqli_connect("$db_host","$db_user","$db_password","$db_name","$db_port"); 
 ?>
